c0056e3766ee84e8cbe3a4f6c5c2163c6cb227a3
hena-ndombele <henandombele2021@gmail.com>
2024-06-26 15:31:10 +0200

    Feat: Création du fichier color de l\'application

---
+++ lib/utils/ColorPages.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorPages{
+
+
+  static const COLOR_PRINCIPALE=Colors.blue;
+  static const COLOR_SECONDAIRE=Colors.red;
+  static const COLOR_BLANCHE=Colors.white;
+  static const COLOR_NOIR=Colors.black;
+
+}
