9398aa41be528a4195303004df835c3493130bfe
hena-ndombele <henandombele2021@gmail.com>
2024-06-26 17:40:16 +0200

    Feat:Creation page liste bluetooth a proximite

--- lib/pages/AppareilPage.dart
+++ lib/pages/AppareilPage.dart
@@ -1,16 +1,66 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-
-class AppareilPage extends StatefulWidget {
-  const AppareilPage({super.key});
-
+class ListeAppareilPage extends StatefulWidget {
   @override
-  State<AppareilPage> createState() => _AppareilPageState();
+  _ListeAppareilPageState createState() => _ListeAppareilPageState();
 }
 
-class _AppareilPageState extends State<AppareilPage> {
+class _ListeAppareilPageState extends State<ListeAppareilPage> {
+  FlutterBluetoothSerial _bluetooth = FlutterBluetoothSerial.instance;
+  List<BluetoothDevice> _devicesList = [];
+  bool _isConnecting = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _requestBluetoothPermissions().then((granted) {
+      if (granted) {
+        _getDevicesList();
+      }
+    });
+  }
+
+  Future<bool> _requestBluetoothPermissions() async {
+    PermissionStatus bluetoothStatus = await Permission.bluetooth.request();
+    PermissionStatus locationStatus = await Permission.locationWhenInUse.request();
+
+    return bluetoothStatus.isGranted && locationStatus.isGranted;
+  }
+
+  Future<void> _getDevicesList() async {
+    try {
+      // Récupère la liste des appareils connectés
+      final connectedDevices = await _bluetooth.getBondedDevices();
+      setState(() {
+        _devicesList = connectedDevices;
+      });
+    } catch (e) {
+      print('Error getting connected devices: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Appareils Bluetooth'),
+      ),
+      body: _devicesList.isEmpty
+          ? Center(child: Text('Aucun appareil Bluetooth connecté'))
+          : ListView.builder(
+        itemCount: _devicesList.length,
+        itemBuilder: (context, index) {
+          final device = _devicesList[index];
+          return ListTile(
+            title: Text(device.name ?? 'Appareil inconnu'),
+            subtitle: Text(device.address),
+          );
+        },
+      ),
+    );
   }
 }
