8230c51b10016d0cd105905a27d40b59897f725d
hena-ndombele <henandombele2021@gmail.com>
2024-06-26 17:08:26 +0200

    Feat: installation de splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:app_bluetooth/utils/ColorPages.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'apps/MonApplication.dart';
 
-void main() {
+void main() async{
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(Duration(seconds: 5));
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle.dark.copyWith(
+        statusBarColor: ColorPages.COLOR_TRANSPARENT),
+  );
+  FlutterNativeSplash.remove();
   runApp(MonApplication());
 }
 
