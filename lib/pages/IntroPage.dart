b9bbd1af8a46b18b01ba2e9870853786643a6646
hena-ndombele <henandombele2021@gmail.com>
2024-06-26 15:30:26 +0200

    Feat: Ajout de la première du lancement de l\'application

---
+++ lib/pages/IntroPage.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+import '../utils/ColorPages.dart';
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 20),
+        child: IntroductionScreen(
+          globalBackgroundColor: ColorPages.COLOR_BLANCHE,
+          scrollPhysics: BouncingScrollPhysics(),
+          pages: [
+            PageViewModel(
+                titleWidget: const Text(
+                  "Mes appareils",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      color: ColorPages.COLOR_PRINCIPALE,
+                      fontFamily: 'Schyler'
+                      // color: Color.fromRGBO(158, 79, 194, 0)
+                      ),
+                ),
+                body: "Rechecher des appareils à proximité...",
+                image: Image.asset(
+                  "images/image1.png",
+                  height: 400,
+                  width: 600,
+                )),
+          ],
+          onDone: () {},
+          next: const Icon(
+            Icons.arrow_forward,
+            color: ColorPages.COLOR_PRINCIPALE,
+          ),
+          done: Container(
+            child: Text(
+              "Bluetooth",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                color: ColorPages.COLOR_PRINCIPALE,
+                fontFamily: 'Schyler',
+              ),
+            ),
+          ),
+          dotsDecorator: DotsDecorator(
+              size: Size.square(7.0),
+              activeSize: Size(15.0, 10.0),
+              color: Colors.black26,
+              activeColor: ColorPages.COLOR_PRINCIPALE,
+              spacing: EdgeInsets.symmetric(horizontal: 3.0),
+              activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25.0),
+              )),
+        ),
+      ),
+    );
+  }
+}
