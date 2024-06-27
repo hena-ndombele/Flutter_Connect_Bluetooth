6218985d140302fbb7c90cc057a05372d52a1e7d
hena-ndombele <henandombele2021@gmail.com>
2024-06-27 13:17:04 +0200

    Feat: Ajout de la couleur gris

--- lib/utils/ColorPages.dart
+++ lib/utils/ColorPages.dart
@@ -8,5 +8,6 @@ class ColorPages{
   static const COLOR_BLANCHE=Colors.white;
   static const COLOR_NOIR=Colors.black;
   static const COLOR_TRANSPARENT=Colors.transparent;
+  static const COLOR_GRIS=Colors.grey;
 
 }
