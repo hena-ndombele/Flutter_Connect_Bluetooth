d6ba58df3b7e860bc6ddbb8f3439eb6b26f95d78
hena-ndombele <henandombele2021@gmail.com>
2024-06-27 12:52:48 +0200

    Feat: Liste des appareils bluetooth Ãa proximitÃe

--- lib/pages/AppareilPage.dart
+++ lib/pages/AppareilPage.dart
@@ -1,45 +1,40 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:permission_handler/permission_handler.dart';
-
 class ListeAppareilPage extends StatefulWidget {
   @override
   _ListeAppareilPageState createState() => _ListeAppareilPageState();
 }
 
 class _ListeAppareilPageState extends State<ListeAppareilPage> {
-  FlutterBluetoothSerial _bluetooth = FlutterBluetoothSerial.instance;
-  List<BluetoothDevice> _devicesList = [];
-  bool _isConnecting = false;
+  List<BluetoothDevice> _devices = [];
 
   @override
   void initState() {
     super.initState();
-    _requestBluetoothPermissions().then((granted) {
-      if (granted) {
-        _getDevicesList();
-      }
-    });
+    _requestBluetoothPermission();
+    _getBluetoothDevices();
   }
 
-  Future<bool> _requestBluetoothPermissions() async {
-    PermissionStatus bluetoothStatus = await Permission.bluetooth.request();
-    PermissionStatus locationStatus = await Permission.locationWhenInUse.request();
-
-    return bluetoothStatus.isGranted && locationStatus.isGranted;
+  void _requestBluetoothPermission() async {
+    await FlutterBluetoothSerial.instance.requestEnable();
   }
 
-  Future<void> _getDevicesList() async {
-    try {
-      // Récupère la liste des appareils connectés
-      final connectedDevices = await _bluetooth.getBondedDevices();
+  Future<bool> _requestBluetoothConnectPermission() async {
+    var status = await Permission.bluetoothConnect.status;
+    if (status != PermissionStatus.granted) {
+      status = await Permission.bluetoothConnect.request();
+    }
+    return status == PermissionStatus.granted;
+  }
+  Future<void> _getBluetoothDevices() async {
+    if (await _requestBluetoothConnectPermission()) {
+      final devices = await FlutterBluetoothSerial.instance.getBondedDevices();
       setState(() {
-        _devicesList = connectedDevices;
+        _devices = devices;
       });
-    } catch (e) {
-      print('Error getting connected devices: $e');
+    } else {
+      // Gérez le cas où l'autorisation n'a pas été accordée
     }
   }
 
@@ -47,19 +42,71 @@ class _ListeAppareilPageState extends State<ListeAppareilPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Appareils Bluetooth'),
+        title: Text('Bluetooth'),
       ),
-      body: _devicesList.isEmpty
-          ? Center(child: Text('Aucun appareil Bluetooth connecté'))
+      body: _devices.isEmpty
+          ? Center(child: CircularProgressIndicator())
           : ListView.builder(
-        itemCount: _devicesList.length,
+        itemCount: _devices.length,
         itemBuilder: (context, index) {
-          final device = _devicesList[index];
+          final device = _devices[index];
           return ListTile(
-            title: Text(device.name ?? 'Appareil inconnu'),
+            title: Text(device.name ?? 'Unknown'),
             subtitle: Text(device.address),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => BluetoothDeviceScreen(device: device),
+                ),
+              );
+            },
           );
         },
+      ),
+    );
+  }
+}
+
+class BluetoothDeviceScreen extends StatefulWidget {
+  final BluetoothDevice device;
+
+  BluetoothDeviceScreen({required this.device});
+
+  @override
+  _BluetoothDeviceScreenState createState() => _BluetoothDeviceScreenState();
+}
+
+class _BluetoothDeviceScreenState extends State<BluetoothDeviceScreen> {
+  bool _isConnected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _connectToDevice();
+  }
+
+  Future<void> _connectToDevice() async {
+    try {
+      await FlutterBluetoothSerial.instance.connect(widget.device);
+      setState(() {
+        _isConnected = true;
+      });
+    } catch (exception) {
+      print('Error connecting to device: $exception');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.device.name ?? 'Unknown'),
+      ),
+      body: Center(
+        child: _isConnected
+            ? Text('Connected to ${widget.device.name}')
+            : CircularProgressIndicator(),
       ),
     );
   }
